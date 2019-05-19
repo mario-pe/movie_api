2c396794e10220a6c2c00b38091f81dd7211e76a
Mario Pe <perkowski.mar@gmail.com>
2019-05-19 18:38:14 +0200

    one more test

--- movie/tests/tests_service.py
+++ movie/tests/tests_service.py
@@ -1,3 +1,6 @@
+from unittest import mock
+from unittest.mock import Mock
+
 from django.test import TestCase
 
 from movie.omdb_service import OmdbService
@@ -12,8 +15,17 @@ class OmdbServiceTest(TestCase):
 
         self.assertDictEqual(result, correct_data)
 
-    def test_should_return_empty_dictionary_if_response_status_is_diffrent_than_200(self):
+    def test_should_return_dictionary_with_incorect_data_when_movie_does_not_exist_in_service(self):
 
         result = OmdbService.get_movie_data('title_doesnt_exist')
 
         self.assertEquals(result, incorrect_data)
+
+    @mock.patch('requests.get')
+    def test_should_return_empty_dictionary_if_respons_status_code_is_defferent_than_200(self, mock_get):
+        mock_resp = Mock(status_code=400)
+        mock_get.return_value = mock_resp
+
+        result = OmdbService.get_movie_data('title_doesnt_exist')
+
+        self.assertEquals(result, {})
